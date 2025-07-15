1466f03b8c2e281aad2eb3d9d5b06b2758e41d56
Netanel kalir <netanelkalir@gmail.com>
2025-07-15 12:03:55 +0300

    templet

--- db.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize, DataTypes } from "sequelize";
-
-export const sequelize = new Sequelize("sequelize_test", "root", '', {
-    logging : false,
-    host: "localhost",
-    dialect: "mysql",
-});
-
